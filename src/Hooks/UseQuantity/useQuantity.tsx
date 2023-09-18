0bb57904ae2e00e44ad570d787d7ecbf37d40d83
lucasfelipedonascimento <lucasfelipe3000@gmail.com>
2023-09-17 21:31:21 -0300

    update quantity hook

--- src/Hooks/UseQuantity/useQuantity.tsx
+++ src/Hooks/UseQuantity/useQuantity.tsx
@@ -1,21 +1,25 @@
 import { useState } from "react";
 
-export function useQuantity() {
-  const [quantityCoffe, setQuantityCoffe] = useState(1);
+interface QuantityProps {
+  quantity: number;
+}
+
+export function useQuantity({ quantity }: QuantityProps) {
+  const [quantityCoffe, setQuantityCoffe] = useState(quantity);
 
   // função para adicionar a quantidade
   const increaseTheQuantity = () => {
     let calc = quantityCoffe + 1;
 
     setQuantityCoffe(calc);
-  }
+  };
 
   // função para diminuir a quantidade
   const decreaseTheQuantity = () => {
     let calc = quantityCoffe - 1;
 
     setQuantityCoffe(calc);
-  }
+  };
 
   return {
     increaseTheQuantity,
@@ -23,3 +27,5 @@ export function useQuantity() {
     quantityCoffe,
   };
 }
+
+import React from "react";
